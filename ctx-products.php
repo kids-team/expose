9afb055f9d10bb35897d15a3b64ac60249e86ae6
Thomas Gollenia <thomas@gollenia.at>
2021-08-30 17:58:22 +0200

    Translations added

--- ctx-products.php
+++ ctx-products.php
@@ -8,7 +8,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     ctx-products
  *
- * @package         create-block
+ * @package         create-products
  */
 
 require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
@@ -19,7 +19,6 @@ $settings = new Contexis\Products\Settings();
 
 // twice?
 $assets = new Contexis\Products\Assets();
-
 
 add_action( 'init', ['Contexis\\Products\\Assets', 'register_block_editor'] );
 add_action( 'wp_enqueue_scripts', ['Contexis\\Products\\Assets', 'register_frontend'] );
@@ -32,11 +31,25 @@ add_filter( 'timber/twig', ["Contexis\\Products\\TwigExtend", "add_to_twig"] );
 $upcoming_block = new Contexis\Products\Blocks\ProductsShop($args);
 $upcoming_block->register();
 
-
-
 register_activation_hook(__FILE__, 'install');
 
 function install() {
-    add_option('ctx_product_options', ['slug'=>'products']);
+    add_option('ctx_product_options', [
+        'slug'=>'products',
+        'mail_subject'=>__("Your Order", "ctx-products"),
+        'mail_content'=>__("Dear #_FORENAME,\n\nThank you for your order. We will send you the following products:\n\n#_PRODUCTLIST\n\nBlessings, your Shop", "ctx-products"),
+        'thanks_head'=>__("Thank you for your order!", "ctx-products"),
+        'thanks_text'=>__("We have recieved your order and we will send it to you as soon as possible", "ctx-products"),
+        'consent'=>__("I consent to all <a href='https://bestlifeonline.com/47-weird-laws-around-world/'>law stuff</a> and allow you to process my data for this very order. All my data will be deleted after shipping.", "ctx-products"),
+    ]);
+}
+
+add_action( 'init', 'ctxproducts_load_textdomain' );
+  
+/**
+ * Load plugin textdomain.
+ */
+function ctxproducts_load_textdomain() {
+  load_plugin_textdomain( 'ctx-products', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
 }
 
