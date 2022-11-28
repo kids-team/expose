3ec0a398f884da8b15eb1f80277c4923998109e2
Thomas Gollenia <thomas@gollenia.at>
2022-11-28 18:44:39 +0100

    changes

--- web/src/components/ProductCard.vue
+++ web/src/components/ProductCard.vue
@@ -1,10 +1,10 @@
 <template>
-     <div  class="card bg-white cursor-pointer" :class="{'card--shadow': attributes.dropShadow, 'card--image-left': attributes.style === 'list', 'card--image-top': attributes.style === 'card'}">
+     <div  class="card bg-white card--image-left" :class="{'card--shadow': attributes.dropShadow, 'card--image-left': attributes.style === 'list', 'card--image-top': attributes.style === 'card'}">
           <span v-if="attributes.showCategory" class="card-label bg-primary text-white"><span v-if="product.categories" v-text="product.categories.length != 0 ? product.categories[0].name : ''"></span></span>
-          <div class="card__image"><img :src="product.images.large"></div>
+          <div class="card__image"><img :src="product.images?.large"></div>
           <div class="card__content">
                 <h5 class="card__title" v-text="product.title.rendered"></h5>
-                <span class="card__text" :class="{'h-16':attributes.style === 'list'}" v-html="product.excerpt.rendered" style="overflow: hidden;"></span>
+                <p class="card__text" v-html="product.excerpt.rendered" style="overflow: hidden;"></p>
                 <div class="card__footer">
                     <div class="card__actions"><a class="button button--link button--primary" @click="$emit('details', $event, product.id)">Details</a></div>
                     <div class="card__supplemental"><span class="button button--link button--pop button--gray button--icon" @click.stop="$emit('select', $event, product.id)"><em class="material-icons">add_shopping_cart</em></span></div>
