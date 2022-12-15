f4c34df83b26446e749c9da8f8d0b755938ed47f
Thomas Gollenia <thomas@gollenia.at>
2022-12-15 17:03:09 +0100

    bugfix

--- web/src/components/ProductCard.vue
+++ web/src/components/ProductCard.vue
@@ -1,25 +1,59 @@
 <template>
-     <div  class="card bg-white card--image-left" :class="{'card--shadow': attributes.dropShadow, 'card--image-left': attributes.style === 'list', 'card--image-top': attributes.style === 'card'}">
-          <span v-if="attributes.showCategory" class="card-label bg-primary text-white"><span v-if="product.categories" v-text="product.categories.length != 0 ? product.categories[0].name : ''"></span></span>
-          <div class="card__image"><img :src="product.images?.large"></div>
-          <div class="card__content">
-                <h5 class="card__title" v-text="product.title.rendered"></h5>
-                <p class="card__text" v-html="product.excerpt.rendered" style="overflow: hidden;"></p>
-                <div class="card__footer">
-                    <div class="card__actions"><a class="button button--link button--primary" @click="$emit('details', $event, product.id)">Details</a></div>
-                    <div class="card__supplemental"><span class="button button--link button--pop button--gray button--icon" @click.stop="$emit('select', $event, product.id)"><em class="material-icons">add_shopping_cart</em></span></div>
-                </div>  
-          </div>
-      </div>
+    <div
+        class="card bg-white card--image-top"
+        :class="{
+            'card--shadow': attributes.dropShadow,
+            'card--image-left': attributes.style === 'list',
+            'card--image-top': attributes.style === 'card',
+        }"
+    >
+        <span
+            v-if="attributes.showCategory"
+            class="card-label bg-primary text-white"
+            ><span
+                v-if="product.categories"
+                v-text="
+                    product.categories.length != 0
+                        ? product.categories[0].name
+                        : ''
+                "
+            ></span
+        ></span>
+        <div class="card__image"><img :src="product.images?.large" /></div>
+        <div class="card__content">
+            <h5 class="card__title" v-text="product.title.rendered"></h5>
+            <p
+                class="card__text"
+                v-html="product.excerpt.rendered"
+                style="overflow: hidden;"
+            ></p>
+            <div class="card__footer">
+                <div class="card__actions">
+                    <a
+                        class="button button--link button--primary"
+                        @click="$emit('details', $event, product.id)"
+                        >Details</a
+                    >
+                </div>
+                <div class="card__supplemental">
+                    <span
+                        class="button button--link button--pop button--gray button--icon"
+                        @click.stop="$emit('select', $event, product.id)"
+                        ><em class="material-icons">add_shopping_cart</em></span
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductCard',
-  props: {
+    name: "ProductCard",
+    props: {
         product: Object,
         attributes: Object,
-        selected: String
-  },
+        selected: String,
+    },
 }
 </script>
