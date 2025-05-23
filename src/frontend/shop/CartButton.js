548a7025499cae0e6629c66c99f3c85d5882d4f0
Thomas Gollenia <thomas@gollenia.at>
2025-05-23 10:28:16 +0200

    reformating

--- src/frontend/shop/CartButton.js
+++ src/frontend/shop/CartButton.js
@@ -1,18 +1,16 @@
-import React, { useState } from '@wordpress/element';
 import MiniCart from './MiniCart';
 
-const CartButton = ( props ) => {
+const CartButton = (props) => {
 	const { cartSize, onClick } = props;
 
-	const [ showMiniCart, setShowMiniCart ] = useState( false );
 	const className = cartSize ? '' : '';
 	return (
 		<div className="ctx-cart-button">
-			<button className={ className } onClick={ onClick } disabled={ cartSize == 0 }>
+			<button className={className} onClick={onClick} disabled={cartSize == 0}>
 				<em className="material-icons material-symbols-outlined">shopping_cart</em>
-				{ !! cartSize && <span className="ctx-cart-button-badge">{ cartSize }</span> }
+				{!!cartSize && <span className="ctx-cart-button-badge">{cartSize}</span>}
 			</button>
-			<MiniCart show={ cartSize > 0 } />
+			<MiniCart show={cartSize > 0} />
 		</div>
 	);
 };
--- src/frontend/shop/CategoryView.js
+++ src/frontend/shop/CategoryView.js
@@ -1,47 +1,41 @@
-import { useContext, useState } from '@wordpress/element';
+import { useContext } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { AppContext } from '../services/context';
 import FakeCards from './FakeCards';
 import ProductCategory from './ProductCategory';
 
-const CategoryView = ( { products, className } ) => {
-	const { state, dispatch } = useContext( AppContext );
+const CategoryView = ({ products, className }) => {
+	const { state, dispatch } = useContext(AppContext);
 
-	const [ showCategory, setShowCategory ] = useState( [] );
-
-	const addToCart = ( product, count = 1 ) => {
-		dispatch( { type: 'ADD_TO_CART', payload: { id: product, count } } );
-	};
-
-	const getProductsByCategory = ( category ) => {
-		return products.filter( ( product ) => {
-			return product.categories.hasOwnProperty( category );
-		} );
+	const getProductsByCategory = (category) => {
+		return products.filter((product) => {
+			return product.categories.hasOwnProperty(category);
+		});
 	};
 
 	return (
 		<>
-			{ state.status === 'SUCCESS' ? (
-				<div className={ className }>
-					<h2>{ __( 'All Products', 'expose' ) }</h2>
-					{ Object.entries( state.categories ).map( ( [ key, value ] ) => {
-						const products = getProductsByCategory( key );
+			{state.status === 'SUCCESS' ? (
+				<div className={className}>
+					<h2>{__('All Products', 'expose')}</h2>
+					{Object.entries(state.categories).map(([key, value]) => {
+						const products = getProductsByCategory(key);
 
-						return <ProductCategory key={ key } title={ value } products={ products } />;
-					} ) }
+						return <ProductCategory key={key} title={value} products={products} />;
+					})}
 				</div>
 			) : (
-				<div className={ className }>
+				<div className={className}>
 					<h4 className="fake-heading"></h4>
 					<div className="ctx-product-grid">
-						<FakeCards count={ 3 } />
+						<FakeCards count={3} />
 					</div>
 					<h4 className="fake-heading"></h4>
 					<div className="ctx-product-grid">
-						<FakeCards count={ 3 } />
+						<FakeCards count={3} />
 					</div>
 				</div>
-			) }
+			)}
 		</>
 	);
 };
