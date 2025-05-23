d0ac4e88d1f14469b197bc4b38fe40cd99901e25
Thomas Gollenia <thomas@gollenia.at>
2025-05-23 10:28:39 +0200

    moved assets to main file

--- expose.php
+++ expose.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     CTX Products
  * Description:     Wordpress Plugin that displays free products like magazines or tracts and let customers place orders without payment.
- * Version:         1.0.1
+ * Version:         1.0.2
  * Author:          Thomas Gollenia
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
@@ -11,21 +11,21 @@
  * @package         create-products
  */
 
-require_once('lib/assets.php');
-require_once('lib/block.php');
+
 require_once('lib/Order.php');
 require_once('lib/Product.php');
 require_once('lib/Settings.php');
+require_once('lib/Update.php');	
 
 
 /**
  * Load plugin textdomain.
  */
-function ctxproducts_load_textdomain() {
+function ctx_products_load_textdomain() {
     load_plugin_textdomain( 'expose', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
 }
 
-add_action( 'init', 'ctxproducts_load_textdomain', 1 );
+add_action( 'init', 'ctx_products_load_textdomain', 1 );
 
 function install() {
     add_option('expose_options', [
@@ -40,7 +40,78 @@ function install() {
 
 register_activation_hook(__FILE__, 'install');
 
+function ctx_products_register_block_editor() {
+	$dir = __DIR__;
+	$script_asset_path = "$dir/build/index.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) return;
+	
+	$script_asset = require( $script_asset_path );
+	
+	wp_register_script(
+			'expose-block-editor',
+			plugins_url( '/build/index.js', __FILE__ ),
+			$script_asset['dependencies'],
+			$script_asset['version']
+	);
+	wp_set_script_translations( 'expose-block-editor', 'expose', plugin_dir_path( __FILE__ ) . '/languages' );
+	
+	wp_enqueue_style(
+				'expose-block-editor',
+				plugins_url( '/build/index.css', __FILE__ ),
+				[],
+				$script_asset['version']
+	);
 
+	$blocks = [
+		'shop',
+		'cart'
+	];
+	
+	foreach($blocks as $block) {
+		register_block_type( __DIR__ . '/build/blocks/'.$block );
+	}         
+}
+
+ function ctx_products_register_frontend() {
+
+	$dir = __DIR__;
+
+	$script_asset_path = "$dir/build/shop.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) return;
+	
+	$script_asset = require( $script_asset_path );
+	
+	wp_register_script(
+		'expose-frontend',
+		plugins_url( '/build/shop.js', __FILE__ ),
+		$script_asset['dependencies'],
+		$script_asset['version'],
+		true
+	);
+	
+
+	wp_register_style(
+		'expose-frontend-style',
+		plugins_url( '/build/style-shop.css', __FILE__ ),
+		[],
+		$script_asset['version']
+	);
+
+	
+}
+
+function ctx_expose_frontend_translation() {
+	wp_set_script_translations( 'expose-frontend', 'expose', plugin_dir_path( __FILE__ ) . '/languages' );
+}
+
+add_action( 'init', 'ctx_products_register_block_editor' );
+add_action( 'init', 'ctx_products_register_frontend' );
+add_action( 'wp_enqueue_scripts', 'ctx_expose_frontend_translation' );
 
   
 
+new \Contexis\Expose\Update(
+	__FILE__,
+	'gollenia',
+	'expose'
+);
