1391727f40ab82e25db228b71d9eeb367caf5ce1
Thomas Gollenia <thomas@gollenia.at>
2026-06-15 12:27:50 +0200

    Refactor Toolbar: replace fragment shorthand with Fragment component for consistency

--- src/blocks/shop/toolbar.js
+++ src/blocks/shop/toolbar.js
@@ -1,17 +1,20 @@
-import { Component } from '@wordpress/element';
-import { BlockControls, AlignmentToolbar } from '@wordpress/block-editor';
+import { AlignmentToolbar, BlockControls } from "@wordpress/block-editor";
+import { Component, Fragment } from "@wordpress/element";
 
 class Toolbar extends Component {
 	render() {
+		const {
+			attributes: { textAlignment },
+			setAttributes,
+		} = this.props;
+
 		return (
-			<>
-				<BlockControls>
-                    <AlignmentToolbar
-                        value={ textAlignment }
-                        onChange={ (event) => setAttributes({ textAlignment: event }) }
-                    />
-				</BlockControls>
-			</>
+			<BlockControls>
+				<AlignmentToolbar
+					value={textAlignment}
+					onChange={(event) => setAttributes({ textAlignment: event })}
+				/>
+			</BlockControls>
 		);
 	}
 }
