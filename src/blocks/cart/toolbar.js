bca50e06975896b175d89437360167abb2834aa8
Thomas Gollenia <thomas@gollenia.at>
2026-06-15 12:28:35 +0200

    Refactor Toolbar: standardize import statements and improve code formatting

--- src/blocks/cart/toolbar.js
+++ src/blocks/cart/toolbar.js
@@ -1,13 +1,18 @@
-import { AlignmentControl, BlockControls, __experimentalLinkControl as LinkControl } from '@wordpress/block-editor';
+import {
+	AlignmentControl,
+	BlockControls,
+	__experimentalLinkControl as LinkControl,
+} from "@wordpress/block-editor";
 
-import { Popover, ToolbarButton } from '@wordpress/components';
+import { Popover, ToolbarButton } from "@wordpress/components";
 
-import { useState } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
-import { link, pullLeft, pullRight, seen, unseen } from '@wordpress/icons';
+import { Fragment, useRef, useState } from "@wordpress/element";
+import { __ } from "@wordpress/i18n";
+import { link, pullLeft, pullRight, seen, unseen } from "@wordpress/icons";
 
-const Toolbar = ( props ) => {
-	const [ isEditingURL, setIsEditingURL ] = useState( false );
+const Toolbar = (props) => {
+	const [isEditingURL, setIsEditingURL] = useState(false);
+	const richTextRef = useRef();
 
 	const {
 		attributes: { url, newTab, rel, iconRight, iconOnly, icon },
@@ -18,76 +23,76 @@ const Toolbar = ( props ) => {
 		<>
 			<BlockControls group="block">
 				<AlignmentControl
-					value={ iconRight ? 'right' : 'left' }
-					onChange={ ( event ) => {
-						setAttributes( {
-							iconRight: event === 'right',
-						} );
-					} }
-					alignmentControls={ [
+					value={iconRight ? "right" : "left"}
+					onChange={(event) => {
+						setAttributes({
+							iconRight: event === "right",
+						});
+					}}
+					alignmentControls={[
 						{
 							icon: pullLeft,
-							title: __( 'Align icon left', 'ctx-blocks' ),
-							align: 'left',
+							title: __("Align icon left", "ctx-blocks"),
+							align: "left",
 						},
 						{
 							icon: pullRight,
-							title: __( 'Align icon right', 'ctx-blocks' ),
-							align: 'right',
+							title: __("Align icon right", "ctx-blocks"),
+							align: "right",
 						},
-					] }
-					label={ __( 'Icon alignment', 'ctx-blocks' ) }
+					]}
+					label={__("Icon alignment", "ctx-blocks")}
 				/>
 
 				<ToolbarButton
 					name="link"
-					icon={ link }
-					title={ __( 'Link', 'ctx-blocks' ) }
-					onClick={ () => setIsEditingURL( true ) }
+					icon={link}
+					title={__("Link", "ctx-blocks")}
+					onClick={() => setIsEditingURL(true)}
 				/>
 
-				{ icon && (
+				{icon && (
 					<ToolbarButton
 						name="iconOnly"
-						icon={ iconOnly ? unseen : seen }
-						title={ __( 'Hide text', 'ctx-blocks' ) }
-						isActive={ iconOnly }
-						onClick={ () => setAttributes( { iconOnly: ! iconOnly } ) }
+						icon={iconOnly ? unseen : seen}
+						title={__("Hide text", "ctx-blocks")}
+						isActive={iconOnly}
+						onClick={() => setAttributes({ iconOnly: !iconOnly })}
 					/>
-				) }
+				)}
 			</BlockControls>
-			{ isEditingURL && (
+			{isEditingURL && (
 				<Popover
 					placement="bottom"
-					onClose={ () => {
-						setIsEditingURL( false );
+					onClose={() => {
+						setIsEditingURL(false);
 						richTextRef.current?.focus();
-					} }
-					focusOnMount={ isEditingURL ? 'firstElement' : false }
-					__unstableSlotName={ '__unstable-block-tools-after' }
+					}}
+					focusOnMount={isEditingURL ? "firstElement" : false}
+					__unstableSlotName={"__unstable-block-tools-after"}
 					shift
 				>
 					<LinkControl
-						value={ { url, opensInNewTab: newTab } }
-						onChange={ ( linkObject ) =>
-							setAttributes( {
+						value={{ url, opensInNewTab: newTab }}
+						onChange={(linkObject) =>
+							setAttributes({
 								rel,
 								url: linkObject.url,
 								newTab: linkObject.opensInNewTab,
 								nofollow: linkObject.noFollow,
-							} )
+							})
 						}
-						onRemove={ () => {
-							setAttributes( {
-								url: '',
+						onRemove={() => {
+							setAttributes({
+								url: "",
 								newTab: false,
 								nofollow: false,
-							} );
-						} }
-						forceIsEditingLink={ isEditingURL }
+							});
+						}}
+						forceIsEditingLink={isEditingURL}
 					/>
 				</Popover>
-			) }
+			)}
 		</>
 	);
 };
