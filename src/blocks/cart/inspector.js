a5f9f452670b07f7338ba27fcc37213303a0830d
Thomas Gollenia <thomas@gollenia.at>
2026-06-15 12:29:09 +0200

    Refactor Inspector: improve formatting and remove unnecessary Fragment wrapper

--- src/blocks/cart/inspector.js
+++ src/blocks/cart/inspector.js
@@ -1,35 +1,34 @@
 /**
  * WordPress dependencies
  */
-import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, PanelRow, TextControl } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { InspectorControls } from "@wordpress/block-editor";
+import { PanelBody, PanelRow, TextControl } from "@wordpress/components";
+import { Fragment } from "@wordpress/element";
+import { __ } from "@wordpress/i18n";
 
 /**
  * Inspector controls
  */
-const Inspector = ( props ) => {
+const Inspector = (props) => {
 	const {
 		attributes: { icon },
 		setAttributes,
 	} = props;
 
 	return (
-		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Icon', 'ctx-blocks' ) } initialOpen={ true }>
-					<PanelRow>
-						<TextControl
-							label={ __( 'Icon', 'ctx-blocks' ) }
-							value={ icon }
-							onChange={ ( value ) => {
-								setAttributes( { icon: value } );
-							} }
-						/>
-					</PanelRow>
-				</PanelBody>
-			</InspectorControls>
-		</>
+		<InspectorControls>
+			<PanelBody title={__("Icon", "ctx-blocks")} initialOpen={true}>
+				<PanelRow>
+					<TextControl
+						label={__("Icon", "ctx-blocks")}
+						value={icon}
+						onChange={(value) => {
+							setAttributes({ icon: value });
+						}}
+					/>
+				</PanelRow>
+			</PanelBody>
+		</InspectorControls>
 	);
 };
 
