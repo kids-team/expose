8229a83a93c4ffd84879ccf16f2f9c09de6abc0d
Thomas Gollenia <thomas@gollenia.at>
2026-06-15 12:29:32 +0200

    Refactor shop.js: streamline DOM element selection and remove unnecessary fragment wrapper

--- src/shop.js
+++ src/shop.js
@@ -3,24 +3,17 @@ import { AppProvider } from './frontend/services/context';
 import Shop from './frontend/shop/Shop';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-	const rootElement = document.getElementById( 'ctx-products-shop' );
-
-	if ( rootElement ) {
+	document.querySelectorAll( '[data-ctx-products-shop]' ).forEach( ( rootElement ) => {
 		const attributes = JSON.parse( rootElement.dataset.attributes );
 		const root = createRoot( rootElement );
 
 		const hash = window.location.hash;
 		const category = hash ? hash.replace( '#', '' ) : '';
 
-		const cartButton = document.getElementById( 'ctx-cart-button' );
-		cartButton?.classList.add( 'has-cart' );
-
 		root.render(
-			<>
-				<AppProvider>
-					<Shop attributes={ attributes } category={ category } />
-				</AppProvider>
-			</>
+			<AppProvider>
+				<Shop attributes={ attributes } category={ category } />
+			</AppProvider>
 		);
-	}
+	} );
 } );
